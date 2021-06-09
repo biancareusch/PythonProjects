cfb22abba297cf04aba8882032be5d59bf34281d
biancareusch <reusch.bianca@gmail.com>
2021-06-09 12:34:34 -0500

    madlibs done

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,15 @@
 # string concatenation
+name = "Bianca"
+
+# Ways to concatenate
+# print("Hi, my name is " + name)
+# print("subscribe to{}".format(name))
+# print(f"subscribe to {name}")
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous person: ")
+madlib = f"Computer programming is so {adj}! It makes me excited all the tie because I love to {verb1}. Stay hydrated " \
+         f"and {verb2} like you are {famous_person}! "
